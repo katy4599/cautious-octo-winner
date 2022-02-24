37da637a45d6a8c60c26a9156abdf862854ca594
katy millard <katy.millard@gmail.com>
2022-02-24 10:11:03 -0600

    Box to red

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ function main() {
   // Get the "my-box" div from the document
   // When someone clicks on the element then change the background color
   // to red.
+  const myBox = document.getElementById('my-box');
+    //myBox.onclick = 'red'
+  
+
+  myBox.addEventListener('click', event => {
+    myBox.style.backgroundColor = 'red';
+  })
 }
+
 
